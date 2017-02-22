eaf4d8bb441d52fa0b6c7a807f45738693f0c1b2
ashe-sparcs <ashe@sparcs.org>
2017-02-23 04:52:50 +0900

    faq search complete

--- SummerSchoolApp/models.py
+++ SummerSchoolApp/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 # Create your models here.
-# class Review
+class Review(models.Model):
+    number = models.IntegerField()
+    student = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    content = models.TextField()
--- SummerSchoolApp/views.py
+++ SummerSchoolApp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Review
+from django.db.models import Q
+import math
 
 
 # Create your views here.
@@ -32,13 +35,31 @@ def contact(request):
     return render(request, 'contact.html', context)
 
 
-def review(request):
+def review(request, page_number):
     context = {}
+    page_number = int(page_number)
+    if 'search_word' in request.GET:
+        search_word = request.GET['search_word']
+        query = Q(student__icontains=search_word) | Q(title__icontains=search_word) | Q(content__icontains=search_word)
+        review_list = Review.objects.filter(query)
+    else:
+        review_list = Review.objects.all()
+    review_list = review_list.order_by('-number')
+    review_list_length = review_list.count()
+    review_list_sliced = review_list[(page_number-1)*10:min([page_number*10, review_list_length])]
+    context['reviews'] = review_list_sliced
+    context['pages'] = [(x+1) for x in range(math.ceil(review_list_length / 10))]
+    context['current_page'] = page_number
+    context['previous_page'] = page_number - 1
+    context['next_page'] = page_number + 1
+    print(review_list_length)
+    print(context['pages'])
     return render(request, 'review.html', context)
 
 
-def review_one(request):
+def review_one(request, number):
     context = {}
+    context['review'] = Review.objects.filter(number=number)[0]
     return render(request, 'review_one.html', context)
 
 
