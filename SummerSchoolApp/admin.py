211ffe9df307a77b1d65c7147cd5d31ee9cf6d78
ashe-sparcs <ashe@sparcs.org>
2017-03-04 16:09:52 +0900

    complete!

--- SummerSchoolApp/admin.py
+++ SummerSchoolApp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django import forms
 
 
 class ImageAdmin(admin.ModelAdmin):
@@ -27,9 +28,23 @@ class QuestionAndAnswerAdmin(admin.ModelAdmin):
     list_display_links = ['id', 'number', 'question']
 
 
+class MyReviewChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return "%s" % obj.title
+
+
+class MyReviewForm(forms.ModelForm):
+    review = MyReviewChoiceField(queryset=Review.objects.all())
+
+    class Meta:
+        model = ReviewImage
+        fields = ['content', 'review']
+
+
 class ReviewImageAdmin(admin.ModelAdmin):
     list_display = ['id', 'content', 'get_review_title']
     list_display_links = ['id', 'get_review_title']
+    form = MyReviewForm
 
 
 # Register your models here.
